1ee08f7ab28f6a0c7e8cab1a3d538db688183d01
Kieran-IDR <kieran@idrsolutions.com>
2021-11-30 09:50:29 +0000

    Update config file to decentralise properties validation

--- src/main/java/com/idrsolutions/microservice/FormVuServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/FormVuServletContextListener.java
@@ -1,9 +1,14 @@
 package com.idrsolutions.microservice;
 
 import javax.servlet.annotation.WebListener;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebListener
 public class FormVuServletContextListener extends BaseServletContextListener {
+
+    private static final Logger LOG = Logger.getLogger(FormVuServletContextListener.class.getName());
 
     @Override
     public String getConfigPath() {
@@ -17,5 +22,24 @@ public class FormVuServletContextListener extends BaseServletContextListener {
     @Override
     public String getConfigName(){
         return "formvu-microservice.properties";
+    }
+
+    @Override
+    public void validateConfigFileValues(final Properties propertiesFile) {
+
+        //service.concurrentConversion
+        validateConcurrentConversions(propertiesFile);
+
+    }
+
+    private static void validateConcurrentConversions(final Properties properties) {
+        String concurrentConversions = properties.getProperty("service.concurrentConversion");
+        if (concurrentConversions == null || concurrentConversions.isEmpty() || !concurrentConversions.matches("\\d+") || Integer.parseInt(concurrentConversions) <= 0) {
+            final int availableProcessors = Runtime.getRuntime().availableProcessors();
+            properties.setProperty("service.concurrentConversion", "" + availableProcessors);
+            final String logDefaultUse = "Properties value for \"service.concurrentConversion\" incorrect, should be a positive integer. Using a value of " + availableProcessors + " based on available processors";
+            LOG.log(Level.WARNING, logDefaultUse);
+        }
+
     }
 }
