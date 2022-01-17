8aea064d4aebbb2c4b2e437262e5a09daac98108
Jacob Lucas <Jacob@IDRSolutions.com>
2022-01-17 12:45:03 +0000

    Added support for the database backend refactor

--- src/main/java/com/idrsolutions/microservice/FormVuServlet.java
+++ src/main/java/com/idrsolutions/microservice/FormVuServlet.java
@@ -20,6 +20,7 @@
  */
 package com.idrsolutions.microservice;
 
+import com.idrsolutions.microservice.db.DBHandler;
 import com.idrsolutions.microservice.utils.DefaultFileServlet;
 import com.idrsolutions.microservice.utils.SettingsValidator;
 import com.idrsolutions.microservice.utils.ZipHelper;
@@ -35,6 +36,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -78,18 +80,24 @@ public class FormVuServlet extends BaseServlet {
      * See API docs for information on how this method communicates via the
      * individual object to the client.
      *
-     * @param individual The individual object associated with this conversion
+     * @param uuid The uuid of the conversion
      * @param params The map of parameters that came with the request
      * @param inputFile The input file
      * @param outputDir The output directory of the converted file
      * @param contextUrl The context that this servlet is running in
      */
     @Override
-    protected void convert(Individual individual, Map<String, String[]> params,
+    protected void convert(String uuid, Map<String, String[]> params,
                            File inputFile, File outputDir, String contextUrl) {
 
-        final Map<String, String> conversionParams = individual.getSettings() != null
-                ? individual.getSettings() : new HashMap<>();
+        final Map<String, String> conversionParams;
+        try {
+            Map<String, String> settings = DBHandler.INSTANCE.getSettings(uuid);
+            conversionParams = settings != null ? settings : new HashMap<>();
+        } catch (final SQLException e) {
+            DBHandler.INSTANCE.setError(uuid, 500, "Database failure");
+            return;
+        }
 
         final String fileName = inputFile.getName();
         final String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
@@ -99,14 +107,14 @@ public class FormVuServlet extends BaseServlet {
         final String outputDirStr = outputDir.getAbsolutePath();
 
         if (!"pdf".equalsIgnoreCase(ext)) {
-            individual.doError(1070, "Internal error processing file - input file must be a PDF Form File");
+            DBHandler.INSTANCE.setError(uuid, 1070, "Internal error processing file - input file must be a PDF Form File");
             return;
         }
 
         //Makes the directory for the output file
         new File(outputDirStr + "/" + fileNameWithoutExt).mkdirs();
 
-        individual.setState("processing");
+        DBHandler.INSTANCE.setState(uuid, "processing");
 
         try {
             final File inFile = new File(inputDir + "/" + fileName);
@@ -128,16 +136,16 @@ public class FormVuServlet extends BaseServlet {
             ZipHelper.zipFolder(outputDirStr + "/" + fileNameWithoutExt,
                     outputDirStr + "/" + fileNameWithoutExt + ".zip");
 
-            final String outputPathInDocroot = individual.getUuid() + "/" + DefaultFileServlet.encodeURI(fileNameWithoutExt);
+            final String outputPathInDocroot = uuid + "/" + DefaultFileServlet.encodeURI(fileNameWithoutExt);
 
-            individual.setValue("previewUrl", contextUrl + "/output/" + outputPathInDocroot + "/form.html");
-            individual.setValue("downloadUrl", contextUrl + "/output/" + outputPathInDocroot + ".zip");
+            DBHandler.INSTANCE.setCustomValue(uuid, "previewUrl", contextUrl + "/output/" + outputPathInDocroot + "/form.html");
+            DBHandler.INSTANCE.setCustomValue(uuid, "downloadUrl", contextUrl + "/output/" + outputPathInDocroot + ".zip");
 
-            individual.setState("processed");
+            DBHandler.INSTANCE.setState(uuid, "processed");
 
         } catch (final Throwable ex) {
             LOG.log(Level.SEVERE, "Exception thrown when converting input", ex);
-            individual.doError(1220, "Exception thrown when converting input" + ex.getMessage());
+            DBHandler.INSTANCE.setError(uuid, 1220, "Exception thrown when converting input" + ex.getMessage());
         }
     }
 
@@ -149,12 +157,12 @@ public class FormVuServlet extends BaseServlet {
      *
      * @param request the request for this conversion
      * @param response the response object for the request
-     * @param individual the individual belonging to this conversion
+     * @param uuid the uuid of this conversion
      * @return true if the settings are parsed and validated successfully, false if not
      */
     @Override
     protected boolean validateRequest(final HttpServletRequest request, final HttpServletResponse response,
-                                      final Individual individual) {
+                                      final String uuid) {
 
         final Map<String, String> settings;
         try {
@@ -205,7 +213,7 @@ public class FormVuServlet extends BaseServlet {
             return false;
         }
 
-        individual.setSettings(settings);
+        request.setAttribute("com.idrsolutions.microservice.settings", settings);
 
         return true;
     }
