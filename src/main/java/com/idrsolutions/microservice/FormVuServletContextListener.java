238c5c974957cf70bd4c4a6700de22f9698034de
Kieran-IDR <kieran@idrsolutions.com>
2023-02-02 11:02:07 +0000

    Move product specific error message into product projects

--- src/main/java/com/idrsolutions/microservice/FormVuServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/FormVuServletContextListener.java
@@ -20,12 +20,18 @@
  */
 package com.idrsolutions.microservice;
 
+import com.idrsolutions.microservice.db.DBHandler;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebListener
 public class FormVuServletContextListener extends BaseServletContextListener {
+
+    private static final Logger LOG = Logger.getLogger(FormVuServletContextListener.class.getName());
 
     @Override
     public String getConfigPath() {
@@ -41,6 +47,12 @@ public class FormVuServletContextListener extends BaseServletContextListener {
         super.contextInitialized(servletContextEvent);
         final Properties propertiesFile = (Properties) servletContextEvent.getServletContext().getAttribute(KEY_PROPERTIES);
         OutputFileServlet.setBasePath(propertiesFile.getProperty(KEY_PROPERTY_OUTPUT_PATH));
+
+        if (DBHandler.isUsingMemoryDatabase()) {
+            final String message = "It is recommended to set your own database instead of using the default internal database as it will allow you to more easily scale the service in the future.\n" +
+                    "More details on the benefits and how to do this can be found here https://support.idrsolutions.com/formvu/tutorials/cloud/options/external-state-database";
+            LOG.log(Level.WARNING, message);
+        }
     }
 
     @Override
